3f0c81c71979b5cbbd56b6f89274aaa2638e974c
Sargi8 <sargisdavtyan888@gmail.com>
2021-10-18 11:34:52 +0300

    beta 1.0.2
    Нав. сетка, зоны прыжка и точки патруля.

--- Assets/MyData/Scripts/Enemy.cs
+++ Assets/MyData/Scripts/Enemy.cs
@@ -1,25 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
 
     public PlayerControler Player;
+    public Transform torse;
+    public float speedRotation = 2f;
+    public NavMeshAgent navMeshAgent;
+    public Transform[] wayPoints;
+    private int m_CurrentWaypointIndex;
 
     private void Awake()
     {
         //нахождение плеера на сцене
-        Player = GameObject.FindObjectOfType<PlayerControler>(); 
+        Player = GameObject.FindObjectOfType<PlayerControler>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        
     }
 
     void Start()
     {
-        Debug.Log($"Hit {Player.name}"); //стрельба по плееру
+        navMeshAgent.SetDestination(wayPoints[0].position);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            m_CurrentWaypointIndex = (m_CurrentWaypointIndex + 1) % wayPoints.Length;
+            navMeshAgent.SetDestination(wayPoints[m_CurrentWaypointIndex].position);
+        }
+
+        Vector3 direction = Player.transform.position - torse.position;
+        Vector3 forwardDirection = new Vector3(direction.x, 0, direction.z);
+
+        Vector3 stepRotation = Vector3.RotateTowards(torse.forward, forwardDirection, speedRotation * Time.fixedDeltaTime, 0.0f);
+
+        torse.rotation = Quaternion.LookRotation(stepRotation);
     }
 }
